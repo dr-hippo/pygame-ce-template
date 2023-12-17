74df30e76a85f1c07f6cea2c6a442fd2c2bcb9f7
Dr.Hippo <epicPythonista@hotmail.com>
2023-12-17 12:18:40 +0800

    Add build script for windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller game.py -F --noconfirm --log-level=WARN ^
+--add-data "assets;assets" --add-data "data;data" ^
+-n "PygameCE-Template" ^
+-p ".venv\Lib\site-packages" % change this path depending on where your venv is % ^
+--noconsole -i "assets\images\sample.jpg"
