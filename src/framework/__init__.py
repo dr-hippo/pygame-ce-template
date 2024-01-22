17a066db91144970163648b7dee896e868d687ec
Dr.Hippo <epicPythonista@hotmail.com>
2024-01-22 14:09:49 +0000

    Remove redundant framework __init__ assignments

--- src/framework/__init__.py
+++ src/framework/__init__.py
@@ -4,8 +4,3 @@ from .entity import Entity
 from .playerdata import PlayerData
 from .playersettings import PlayerSettings
 from .scene import Scene
-
-Scene = Scene
-Entity = Entity
-PlayerData = PlayerData
-PlayerSettings = PlayerSettings
